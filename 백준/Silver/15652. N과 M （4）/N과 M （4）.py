8f452d1bdfc2a60552b64900899921a22985c2dd
yichoya <111272770+yichoya@users.noreply.github.com>
2023-03-12 17:00:31 +0900

    [Silver III] Title: N과 M (4), Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,17 @@
+import sys
+
+n, m = map(int,sys.stdin.readline().split())
+cnt = 0
+res = []
+
+def combination(cnt, n, m, start):
+    if cnt == m:
+        print(" ".join(map(str, res)))
+        return
+    for i in range(start, n + 1):
+        # if (i not in res):
+            res.append(i)
+            combination(cnt + 1, n, m, i)
+            res.pop()
+
+combination(cnt, n, m, 1)
