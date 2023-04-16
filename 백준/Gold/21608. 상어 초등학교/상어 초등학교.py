0ddcd2795c418b7efb0e4bc56b4972995894b537
yichoya <111272770+yichoya@users.noreply.github.com>
2023-04-16 13:24:53 +0900

    [Gold V] Title: 상어 초등학교, Time: 276 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Gold/21608. 상어 초등학교/상어 초등학교.py
@@ -0,0 +1,61 @@
+import sys
+
+n = int(sys.stdin.readline())
+students = [list(map(int, sys.stdin.readline().split())) for _ in range(n ** 2)]
+seat = [[0] * n for _ in range(n)]
+
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
+
+for i in range(n ** 2):
+    student = students[i][0]
+    tmpFriend = -1
+    tmpBlank = -1
+    tmpR = 0
+    tmpC = 0
+    for r in range(n):
+        for c in range(n):
+            if seat[r][c] == 0:
+                friend = 0
+                blank = 0
+                for j in range(4):
+                    nr = r + dr[j]
+                    nc = c + dc[j]
+                    if 0 <= nr < n and 0 <= nc < n:
+                        if seat[nr][nc] in students[i][1:]:
+                            friend += 1
+                        if seat[nr][nc] == 0:
+                            blank += 1
+                if tmpFriend < friend:
+                    tmpFriend = friend
+                    tmpBlank = blank
+                    tmpR = r
+                    tmpC = c
+                elif tmpFriend == friend:
+                    if tmpBlank < blank:
+                        tmpBlank = blank
+                        tmpR = r
+                        tmpC = c
+                    elif tmpBlank == blank:
+                        if tmpR > r:
+                            tmpR = r
+                            tmpC = c
+                        elif tmpR == r:
+                            if tmpC > c:
+                                tmpC = c
+    seat[tmpR][tmpC] = student
+
+students.sort()
+res = 0
+for i in range(n):
+    for j in range(n):
+        score = 0
+        for k in range(4):
+            nr = i + dr[k]
+            nc = j + dc[k]
+            if 0 <= nr < n and 0 <= nc < n:
+                if seat[nr][nc] in students[seat[i][j] - 1]:
+                    score += 1
+        if score > 0:
+            res += 10 ** (score - 1)
+print(res)
