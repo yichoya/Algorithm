237e036af1b44a99b0e10e36137f2c307b3d6c10
yichoya <111272770+yichoya@users.noreply.github.com>
2023-05-04 17:27:16 +0900

    [Gold IV] Title: 공유기 설치, Time: 552 ms, Memory: 38984 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -0,0 +1,26 @@
+import sys
+
+N, C = map(int, sys.stdin.readline().split())
+houses = [int(sys.stdin.readline().rstrip()) for _ in range(N)]
+houses.sort()
+
+start = 1
+end = houses[-1] - houses[0]
+res = 0
+
+while(start <= end):
+    mid = int((start + end) // 2)
+    router = houses[0]
+    cnt = 1
+
+    for i in range(1, N):
+        if houses[i] >= router + mid:
+            router = houses[i]
+            cnt += 1
+            
+    if cnt >= C: 
+        start = mid + 1
+        res = mid
+    else:
+        end = mid - 1
+print(res)
