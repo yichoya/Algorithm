2644cc29563ef43788cf60fab9af67b934d299aa
yichoya <111272770+yichoya@users.noreply.github.com>
2023-05-22 16:59:26 +0900

    [Gold V] Title: LCS, Time: 524 ms, Memory: 55712 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,15 @@
+import sys
+
+string1 = [0] + list(sys.stdin.readline().rstrip())
+string2 = [0] + list(sys.stdin.readline().rstrip())
+
+dp = [[0] * len(string2) for _ in range(len(string1))]
+
+for i in range(1, len(string1)):
+    for j in range(1, len(string2)):
+        if string1[i] == string2[j]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+print(dp[-1][-1])
