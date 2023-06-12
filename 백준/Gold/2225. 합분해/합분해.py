0a0376f79a0cc88260d6ddee94e5f9dd292db209
yichoya <111272770+yichoya@users.noreply.github.com>
2023-06-12 23:28:11 +0900

    [Gold V] Title: 합분해, Time: 56 ms, Memory: 32276 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -0,0 +1,12 @@
+import sys
+n, k = map(int, sys.stdin.readline().split())
+
+dp = [[0] * (n + 1) for _ in range(k + 1)]
+dp[0][0] = 1
+
+for i in range(1, k + 1):
+    for j in range(n + 1):
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        dp[i][j] %= 1000000000
+
+print(dp[k][n])
