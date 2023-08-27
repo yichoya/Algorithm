0782998ae5602afab90b1df85f8d758fcd63977b
yichoya <111272770+yichoya@users.noreply.github.com>
2023-08-28 00:31:52 +0900

    [Gold IV] Title: 게리맨더링, Time: 160 ms, Memory: 15832 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -2,109 +2,104 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int N;
+    static int[] population;
+    static boolean[] isSelected, visited;
+    static List<ArrayList<Integer>> nodeInfo; // 지역 연결 정보를 담는 배열
+    static int ans = Integer.MAX_VALUE;
 
-	static int N;
-	static int peoples[]; // 구역별 인구수
-	static List<ArrayList<Integer>> graph;
-	static boolean selected[];
-	static boolean visited[];
-	static int res;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine()); // 지역 개수
-		res = Integer.MAX_VALUE; // 인구 차이(정답)
-		peoples = new int[N]; // 지역별 인구 수
-		selected = new boolean[N]; // 부분집합 만들 때 사용
+        N = Integer.parseInt(br.readLine());
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) // 지역별 인구 수 입력
-			peoples[i] = Integer.parseInt(st.nextToken());
+        population = new int[N];
+        nodeInfo = new ArrayList<>();
 
-		graph = new ArrayList<>();
-		for (int i = 0; i < N; i++) {
-			graph.add(new ArrayList<Integer>());
-		}
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            population[i] = Integer.parseInt(st.nextToken());
+        }
 
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			int cnt = Integer.parseInt(st.nextToken()); // 인접 구역 수
-			for (int j = 0; j < cnt; j++) {
-				int num = Integer.parseInt(st.nextToken());
-				graph.get(i).add(num - 1);
-			}
-		}
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int e = Integer.parseInt(st.nextToken());
+            nodeInfo.add(new ArrayList<>());
+            for (int j = 0; j < e; j++) {
+                int nodeNo = Integer.parseInt(st.nextToken());
+                nodeInfo.get(i).add(nodeNo - 1);
+            }
+        }
 
-		divide(0);
-		if (res == Integer.MAX_VALUE)
-			res = -1;
-		System.out.println(res);
+        isSelected = new boolean[N];
+        subSet(0);
+        if (ans == Integer.MAX_VALUE)
+            ans = -1;
+        System.out.println(ans);
 
-	}
+    }
 
-	private static void divide(int idx) { // 1. 선거구 나누기
-		if (idx == N) {
-			List<Integer> aList = new ArrayList<>();
-			List<Integer> bList = new ArrayList<>();
-			for (int i = 0; i < N; i++) {
-				if (selected[i])
-					aList.add(i);
-				else
-					bList.add(i);
-			}
-			if (aList.size() == 0 || bList.size() == 0) // 한 지역에 몰빵 X
-				return;
-			
-			if (check(aList) && check(bList)) { // 두 구역이 각각 연결되었는지 확인
-				getPeopleDiff(); // 인구차 구하기
-			}
-			return;
-		}
+    static int count(List<Integer> a) { // a 선거구와 b 선거구의 인구 수 차이 계산
+        int sumA = 0, sumB = 0;
+        for (int i = 0; i < N; i++) {
+            if (isSelected[i]) {
+                sumA += population[i];
+            } else sumB += population[i];
+        }
 
-		selected[idx] = true;
-		divide(idx + 1);
-		selected[idx] = false;
-		divide(idx + 1);
+        return Math.abs(sumA - sumB);
+    }
 
-	}
+    static boolean check(List<Integer> a) {  // 같은 선거구끼리 다 연결되어있는지 확인
+        //List<ArrayList<Integer>> aList = Arrays.asList(a);  // ArrayList -> List 변환
+        Queue<Integer> q = new ArrayDeque<>();
+        visited = new boolean[N];
+        int cntArea = 1;
 
-	private static boolean check(List<Integer> list) {
+        q.offer(a.get(0));
+        visited[a.get(0)] = true;
 
-		Queue<Integer> q = new LinkedList<>();
-		visited = new boolean[N];
-		visited[list.get(0)] = true;
-		q.offer(list.get(0));
-		
-		int count = 1; // 방문한 지역 개수
-		while (!q.isEmpty()) {
-			int cur = q.poll();
-			for (int i = 0; i < graph.get(cur).size(); i++) {
-				int y = graph.get(cur).get(i);
-				if(list.contains(y) && !visited[y]) { // 선거구에 해당하고, 아직 미방문
-					q.offer(y);
-					visited[y] = true;
-					count ++;
-				}
-			}
-		}
-		if(count==list.size()) // 선거구에 해당하는 지역수와 방문한 지역수가 같은 경우
-			return true;
-		else
-			return false;
-	}
+        while(!q.isEmpty()) {
+            int cur = q.poll();
+            for (int i = 0; i < nodeInfo.get(cur).size(); i++) {
+                int node = nodeInfo.get(cur).get(i);
 
+                if (!visited[node] && a.contains(node)) {
+                    q.offer(node);
+                    visited[node] = true;
+                    cntArea++;
+                }
+            }
+        }
 
-	private static void getPeopleDiff() { // 3. 선거구의 인구 차 구하기
-		// a구역 사람수
-		int pA = 0, pB = 0;
-		for (int i = 0; i < N; i++) {
-			if (selected[i])
-				pA += peoples[i];
-			else
-				pB += peoples[i];
-		}
-		int diff = Math.abs(pA - pB);
-		res = Math.min(res, diff);
-	}
+        if (cntArea == a.size()) return true;
+        return false;
+    }
 
+    static void subSet(int cnt) {  // 부분집합으로 선거구 a, b 나누기
+        if(cnt == N) {
+            List<Integer> a = new ArrayList<>();
+            List<Integer> b = new ArrayList<>();
+
+            for (int i = 0; i < N; i++) {
+                if (isSelected[i] ? a.add(i): b.add(i));
+            }
+            
+            if (a.size() == 0 || b.size() == 0)  {
+                return;
+            }
+
+            // a끼리 b끼리 다 연결되어있는지 확인하는 함수 호출
+            if (check(a) && check(b)) {
+                ans = Math.min(ans, count(a));
+            }
+            return;
+        }
+
+        isSelected[cnt] = true;
+        subSet(cnt + 1);
+        isSelected[cnt] = false;
+        subSet(cnt + 1);
+    }
 }
