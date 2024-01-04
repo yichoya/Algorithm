93e27a4390ebbc366654a56eea6d20a00724abb7
yichoya <111272770+yichoya@users.noreply.github.com>
2024-01-05 00:31:42 +0900

    [Bronze I] Title: 나무 조각, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2947. 나무 조각/나무 조각.py
@@ -0,0 +1,13 @@
+import sys
+
+nums = list(map(int, sys.stdin.readline().split(' ')))
+
+while True:
+  for i in range(len(nums) - 1):
+    if nums[i] > nums[i + 1]:
+      temp = nums[i]
+      nums[i] = nums[i + 1]
+      nums[i + 1] = temp
+      print(' '.join(map(str, nums)))
+
+  if nums == [1, 2, 3, 4, 5]: break
