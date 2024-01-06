c4dd7d7f748edce46236da30de11354d60e733ef
yichoya <111272770+yichoya@users.noreply.github.com>
2024-01-06 16:06:10 +0900

    [Gold V] Title: 등차수열과 쿼리, Time: 48 ms, Memory: 31120 KB, Score: 10 point -BaekjoonHub

---
+++ 백준/Gold/23888. 등차수열과 쿼리/등차수열과 쿼리.py
@@ -0,0 +1,23 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+def getGCD(x, y):
+    if x % y == 0:
+        return y
+    return getGCD(y, x % y)
+    
+a, d = map(int, input().split())
+q = int(input())
+for _ in range(q):
+    c, start, end = map(int, input().split())
+
+    if c == 1:
+        n = end + 1 - start
+        print(int(n * (2 * a + (start + end - 2) * d) / 2)) 
+    else:
+        gcd = a + (start - 1) * d
+        for i in range(start, end):
+            cur = a + (i - 1) * d
+            gcd = getGCD(gcd, cur + d)
+        print(gcd)
