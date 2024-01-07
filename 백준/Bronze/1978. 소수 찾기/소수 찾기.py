4065115bfb53b0240d8188bb758e253058ea012a
yichoya <111272770+yichoya@users.noreply.github.com>
2024-01-07 23:49:00 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = list(map(int, input().split()))
+cnt = 0
+
+for x in arr:
+  for i in range(2, x + 1):
+    if x % i == 0:
+      if x == i:
+        cnt += 1
+      break
+
+print(cnt)
