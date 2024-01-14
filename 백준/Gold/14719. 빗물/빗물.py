e41cb1bb245e2b356ce50ba7859e65f203a6f927
yichoya <111272770+yichoya@users.noreply.github.com>
2024-01-14 18:24:18 +0900

    [Gold V] Title: 빗물, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -2,20 +2,12 @@ import sys
 
 H, W = map(int, sys.stdin.readline().split())
 blocks = list(map(int, sys.stdin.readline().split()))
-
-water = 0
-
-for i in range(1, W-1):
-    maxL = max(blocks[:i]) 
-    maxR = max(blocks[i+1:]) 
+ans = 0
+for i in range(1, W - 1):
+    maxL = max(blocks[ : i]) 
+    maxR = max(blocks[i + 1 : ]) 
     
     standard = min(maxL, maxR)
-    
     if blocks[i] < standard:
-        water += standard - blocks[i]
-
-print(water)
-
-#3 6
-#5 4 1 3 1 2
-#ans = 3
+        ans += standard - blocks[i]
+print(ans)
