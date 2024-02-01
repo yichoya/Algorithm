0bcbae109c013884f336048000b409f2cb08a69b
yichoya <111272770+yichoya@users.noreply.github.com>
2024-02-02 00:04:25 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,17 +1,14 @@
 import sys
-
-n, m = map(int,sys.stdin.readline().split())
-cnt = 0
-res = []
-
-def combination(cnt, n, m, start):
-    if cnt == m:
-        print(" ".join(map(str, res)))
+input = sys.stdin.readline
+n, m = map(int, input().split())
+select = []
+def recur(x):
+    if len(select) == m:
+        print(*select)
         return
-    for i in range(start, n + 1):
-        #if (i not in res):
-            res.append(i)
-            combination(cnt + 1, n, m, i + 1)
-            res.pop()
-
-combination(cnt, n, m, 1)
+    for i in range(x, n + 1):
+        if i not in select:
+            select.append(i)
+            recur(i + 1)
+            select.pop()
+recur(1)
