32bd6949f6b7b6f3088d9d58a292cb495c053349
yichoya <111272770+yichoya@users.noreply.github.com>
2024-02-19 00:58:53 +0900

    [Gold V] Title: 평범한 배낭, Time: 3380 ms, Memory: 34972 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,23 +1,13 @@
 import sys
-
 N, K = map(int, sys.stdin.readline().rstrip().split())
-bag = [(0, 0)]
+bag = []
 for _ in range(N):
     W, V = map(int, sys.stdin.readline().rstrip().split())
     bag.append((W, V))
 
-# v값이 저장되는 이차원배열    
-dp = [[0] * (K + 1) for _ in range(N + 1)]
-
-for i in range(1, N + 1):
-    w, v = bag[i]
-    for j in range(1, K + 1):
-        if w > j:
-            # i번째 물건 안넣음
-            dp[i][j] = dp[i - 1][j]
-        else:
-            # max(i번째 물건 안넣고 j의 무게를 만드는 경우, i번째 물건을 넣고 j의 무게를 만드는 경우)
-            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w] + v)
-
-print(dp[N][K])
-#print(dp)
+dp = [0 for _ in range(K + 1)] # 가치 체크 배열
+for b in bag:
+    w, v = b
+    for i in range(K, w - 1, -1):
+        dp[i] = max(dp[i], dp[i - w] + v)
+print(dp[-1])
