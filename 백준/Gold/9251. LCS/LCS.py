b9ddd1b5733d8625894b471061cb192994930368
yichoya <111272770+yichoya@users.noreply.github.com>
2024-02-25 17:57:50 +0900

    [Gold V] Title: LCS, Time: 340 ms, Memory: 55708 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,15 +1,21 @@
-import sys
+str1 = input()
+str2 = input()
+dp = [[0] * (len(str2) + 1) for _ in range(len(str1) + 1)]
 
-string1 = [0] + list(sys.stdin.readline().rstrip())
-string2 = [0] + list(sys.stdin.readline().rstrip())
+def lcs(str1, str2):
+    for i in range(1, len(str1) + 1):
+        a = str1[i - 1]
+        for j in range(1, len(str2) + 1):
+            b = str2[j - 1]
 
-dp = [[0] * len(string2) for _ in range(len(string1))]
+            if a == b:
+                # 지금까지의 최대 공통 부분수열에 1을 더함
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                # 이전의 최대 공통 부분수열은 계속해서 유지
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+    return dp[len(str1)][len(str2)]
 
-for i in range(1, len(string1)):
-    for j in range(1, len(string2)):
-        if string1[i] == string2[j]:
-            dp[i][j] = dp[i - 1][j - 1] + 1
-        else:
-            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-
-print(dp[-1][-1])
+res = lcs(str1, str2)
+print(res)
+#print(dp)
