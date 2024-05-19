30a066cfea468571bc4c4df772650f2929648767
yichoya <111272770+yichoya@users.noreply.github.com>
2024-05-19 09:15:44 +0900

    [Silver I] Title: RGB거리, Time: 80 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -5,8 +5,8 @@ for _ in range(n):
     rgb.append(list(map(int, input().strip().split())))
 
 for i in range(1, n):
-    rgb[i][0] = min(rgb[i - 1][1], rgb[i - 1][2]) + rgb[i][0]  #red
-    rgb[i][1] = min(rgb[i - 1][0], rgb[i - 1][2]) + rgb[i][1]  #green
-    rgb[i][2] = min(rgb[i - 1][0], rgb[i - 1][1]) + rgb[i][2]  #blue
+    rgb[i][0] = min(rgb[i - 1][1], rgb[i - 1][2]) + rgb[i][0]
+    rgb[i][1] = min(rgb[i - 1][0], rgb[i - 1][2]) + rgb[i][1]
+    rgb[i][2] = min(rgb[i - 1][0], rgb[i - 1][1]) + rgb[i][2]
 
 print(min(rgb[n - 1]))
