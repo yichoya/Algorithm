22fae50025f1e97b0833a9cfb04eb751765d98aa
yichoya <111272770+yichoya@users.noreply.github.com>
2024-08-15 16:45:42 +0900

    [Bronze III] Title: 완전 제곱수, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/6131. 완전 제곱수/완전 제곱수.py
+++ 백준/Bronze/6131. 완전 제곱수/완전 제곱수.py
@@ -1,10 +1,9 @@
 import sys
-input = sys.stdin.readline
 
-N = int(input())
+n = int(sys.stdin.readline())
 cnt = 0
 for b in range(1, 501):
     for a in range(b, 501):
-        if a**2 == b**2 + N:
+        if a**2 == b**2 + n:
             cnt += 1
 print(cnt)
