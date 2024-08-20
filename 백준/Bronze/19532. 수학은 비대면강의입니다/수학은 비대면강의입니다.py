ce911094b5254e56d61b06567bd3de5cfcda86f8
yichoya <111272770+yichoya@users.noreply.github.com>
2024-08-20 10:28:09 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 644 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -1,7 +1,6 @@
 import sys
-input = sys.stdin.readline
 
-a, b, c, d, e, f = map(int, input().split())
+a, b, c, d, e, f = map(int, sys.stdin.readline().split())
 for x in range(-999, 1000):
     for y in range(-999, 1000):
         if a * x + b * y == c:
