5854d990abd28d8c74dd719f37df86684bdc4d09
yichoya <111272770+yichoya@users.noreply.github.com>
2024-09-02 18:31:45 +0900

    [Gold II] Title: 카누 선수, Time: 3464 ms, Memory: 258204 KB -BaekjoonHub

--- 백준/Gold/9007. 카누 선수/카누 선수.py
+++ 백준/Gold/9007. 카누 선수/카누 선수.py
@@ -1,32 +1,33 @@
 import sys
-input = sys.stdin.readline
-T = int(input())
-for _ in range(T):
-    k, n = map(int, input().split())  # k는 보트가 원하는 무게, n은 각 반당 인원
-    arr = [list(map(int, input().split())) for _ in range(4)]  # 4 반의 학생들의 몸무게
-    arr_1 = []
-    arr_2 = []
+t = int(sys.stdin.readline())
+for _ in range(t):
+    k, n = map(int, sys.stdin.readline().split())
+    weights = [list(map(int, sys.stdin.readline().split())) for _ in range(4)]
+
+    # weightsA: class1, class2에서 조합할 수 있는 몸무게의 합
+    # weightsB: class3, class4에서 조합할 수 있는 몸무게의 합
+    weightsA, weightsB = [], []
     for i in range(n):
         for j in range(n):
-            arr_1.append(arr[0][i] + arr[1][j])  # 1, 2번째 배열 더하기
-            arr_2.append(arr[2][i] + arr[3][j])  # 3, 4번째 배열 더하기
-    arr_1.sort()
-    arr_2.sort()
-    
-    start = 0
-    end = len(arr_1) - 1
-    result = arr_1[start] + arr_2[end]
-    while start < len(arr_1) and end >= 0:
-        tmp = arr_1[start] + arr_2[end]
-        if abs(k - result) > abs(k - tmp):
-            result = tmp
-        elif abs(k - result) == abs(k - tmp):
-            result = min(tmp, result)
-        
+            weightsA.append(weights[0][i] + weights[1][j])
+            weightsB.append(weights[2][i] + weights[3][j])
+    weightsA.sort()
+    weightsB.sort()
+
+    start, end = 0, n**2 - 1
+    res = weightsA[start] + weightsB[end]
+    while start < n**2 and end > -1:
+        tmp = weightsA[start] + weightsB[end]
+        # 절대값이 같은 경우 -> 더 작은값 채택
+        if abs(k - tmp) == abs(k - res):
+            res = min(res, tmp)
+        elif abs(k - tmp) < abs(k - res):
+            res = tmp
+
         if tmp < k:
             start += 1
         elif tmp > k:
             end -= 1
         else:
             break
-    print(result)
+    print(res)
