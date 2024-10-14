77f4c6aae8961d413b8157f7bad0b573bdc6e671
yichoya <111272770+yichoya@users.noreply.github.com>
2024-10-14 16:00:42 +0900

    [Gold V] Title: 평범한 배낭, Time: 1656 ms, Memory: 134092 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,17 +1,18 @@
 import sys
 n, k = map(int, sys.stdin.readline().split())
-things = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+stuff = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 dp = [[-1] * 100001 for _ in range(n)]
 
-def recur(cur, w):
-    global things
-    if w > k:
-        return -123123123
+def recur(cur, weight):    # cur에서 선택할 수 있는 최대 가치 리턴
+
+    if weight > k:
+        return -1234567890
     if cur == n:
         return 0
-    if dp[cur][w] != -1:
-        return dp[cur][w]
-    dp[cur][w] = max(recur(cur + 1, w + things[cur][0]) + things[cur][1], recur(cur + 1, w))
-    return dp[cur][w]
+    if dp[cur][weight] != -1:
+        return dp[cur][weight]
+
+    dp[cur][weight] = max(recur(cur + 1, weight), recur(cur + 1, weight + stuff[cur][0]) + stuff[cur][1])
+    return dp[cur][weight]
 
 print(recur(0, 0))
