6313e0563938f690e5470ed1b8d9b7aa0a8b9ea2
yichoya <111272770+yichoya@users.noreply.github.com>
2024-10-23 17:57:03 +0900

    [Silver III] Title: 바이러스, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,24 +1,21 @@
-n = int(input())
-m = int(input())
-
-# 컴퓨터 연결 상태를 2차원 리스트로 저장
-# 컴퓨터의 번호를 인덱스로 사용하기 위해 길이를 n + 1로 설정
-data = [[] * (n + 1) for _ in range(n + 1)]
-for _ in range(m):
-    a, b = map(int, input().split())
-    data[a].append(b)
-    data[b].append(a)
-
+import sys
+n = int(sys.stdin.readline().rstrip())
+l = int(sys.stdin.readline().rstrip())
+graph = [[] for _ in range(n + 1)]
+for _ in range(l):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
 visited = [False] * (n + 1)
 res = 0
 
-def dfs(start):
+def dfs(node):
     global res
-    visited[start] = True
-    for i in data[start]:
-        if visited[i] == False:
-            dfs(i)
+    visited[node] = True
+    for i in graph[node]:
+        if not visited[i]:
             res += 1
+            dfs(i)
 
 dfs(1)
 print(res)
