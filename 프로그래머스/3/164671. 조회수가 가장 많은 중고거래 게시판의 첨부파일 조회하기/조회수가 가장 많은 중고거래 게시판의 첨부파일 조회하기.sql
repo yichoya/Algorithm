2bcdb8847e5526af000ba1726805b3e8d1245544
yichoya <111272770+yichoya@users.noreply.github.com>
2024-11-01 22:21:31 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT CONCAT('/home/grep/src/', F.BOARD_ID, '/', F.FILE_ID, F.FILE_NAME, F.FILE_EXT) FILE_PATH
+FROM USED_GOODS_FILE F
+WHERE F.BOARD_ID = (SELECT BOARD_ID
+                    FROM USED_GOODS_BOARD
+                    ORDER BY VIEWS DESC
+                    LIMIT 1)
+ORDER BY FILE_ID DESC
